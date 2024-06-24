16b82b21c9e061481ad26879b014c377e32f57dc
beibanezv <166031898+beibanezv@users.noreply.github.com>
2024-06-24 11:32:48 -0400

    Add files via upload

--- funciones.py
+++ funciones.py
@@ -9,8 +9,8 @@ inventario = [
 
 def agregar_producto():
     codigo = input("Ingrese el código del producto a ingresar en el inventario: ")
-    nombre = input("Ingrese el nombre del producto a ingresar: ")
-    cantidad = input("Ingrese el stock del producto a ingresar: ")
+    nombre = input("Ingrese el nombre del producto a ingresar: ").capitalize()
+    cantidad = int(input("Ingrese el stock del producto a ingresar: "))
     precio = int(input("Ingrese el precio del producto a ingresar: "))
 
     producto = {
@@ -20,13 +20,23 @@ def agregar_producto():
         'PRECIO': precio
     }
 
+    inventario.append(producto)
+    print(f"Se han agregado {cantidad} {nombre} ({codigo}) con un valor unitario de {precio} exitosamente. ")
 def ver_productos():
-    print()
+    print("** Usted eligio opción ver lista de productos **")
+    for producto in inventario:
+        print(f"{producto['CODIGO']} - {producto['NOMBRE']} - {producto['CANTIDAD']} - {producto['PRECIO']}\n")
+
 def modificar_producto():
     print()
+
 def eliminar_producto():
-    print()
+    producto_remover = input("Ingrese el nombre del producto que quiere quitar del inventario: ").capitalize()
+
 def guardar_archivo():
-    print()
+    with open('inventario.txt', 'w', encoding='utf-8') as f:
+        for producto in inventario:
+            f.write(f"{producto['CODIGO']} - {producto['NOMBRE']} - {producto['CANTIDAD']} - {producto['PRECIO']}\n")
+
 def menu():
     print()
--- main.py
+++ main.py
@@ -1,1 +1,3 @@
 import funciones
+
+print("--- Bienvenido al programa de gestión de inventario de la tienda 'SCCOOBY DOO' ---")
