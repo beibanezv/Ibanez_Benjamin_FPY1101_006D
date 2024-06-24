90a4391fee40df75813dfc9f921787e53d13ddbb
beibanezv <166031898+beibanezv@users.noreply.github.com>
2024-06-24 12:40:39 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -2,21 +2,24 @@ import funciones
 import time
 
 print("--- Bienvenido al programa de gestión de inventario de la tienda 'SCOOBY DOO' ---")
-menu_salir = input("Ingrese 'm' si desea abrir el menú de opciones o ingrese 'x' para salir del programa.")
-
-while menu_salir not in ["m","x"]:
-    print("No se ingresó una opción válida.")
+while True:
     menu_salir = input("Ingrese 'm' si desea abrir el menú de opciones o ingrese 'x' para salir del programa.")
 
-if menu_salir == "m":
-    flag = False
-    while True:
-        funciones.menu()
+    while menu_salir not in ["m","x"]:
+        print("No se ingresó una opción válida.")
+        menu_salir = input("Ingrese 'm' si desea abrir el menú de opciones o ingrese 'x' para salir del programa.")
 
-elif menu_salir == "x":
-    print("************************")
-    print("Saliendo del programa...")
-    print("************************")
-    time.sleep(2)
-    flag = False
+    if menu_salir == "m":
+        while True:
+            funciones.menu()
+            break
+
+    elif menu_salir == "x":
+        print("************************")
+        print("Saliendo del programa...")
+        print("************************")
+        time.sleep(2)
+        flag = False
+        break
+
 
