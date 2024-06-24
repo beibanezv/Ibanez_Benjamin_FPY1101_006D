f2d6fe8a537770385a94eadf304976ff8b9c767e
beibanezv <166031898+beibanezv@users.noreply.github.com>
2024-06-24 12:21:46 -0400

    Cambios en menu y salir agregado a funciones

--- funciones.py
+++ funciones.py
@@ -1,3 +1,5 @@
+import time
+
 inventario = [
     {
     'CODIGO':'CODIGO',
@@ -22,16 +24,23 @@ def agregar_producto():
 
     inventario.append(producto)
     print(f"Se han agregado {cantidad} {nombre} ({codigo}) con un valor unitario de {precio} exitosamente. ")
+
 def ver_productos():
     print("** Usted eligio opción ver lista de productos **")
     for producto in inventario:
         print(f"{producto['CODIGO']} - {producto['NOMBRE']} - {producto['CANTIDAD']} - {producto['PRECIO']}\n")
 
 def modificar_producto():
-    print()
+    producto_modificacion = input("Ingrese el producto a modificar  del inventario")
+    dato = input("Ingrese el valor que desea cambiar")
+
 
 def eliminar_producto():
     producto_remover = input("Ingrese el nombre del producto que quiere quitar del inventario: ").capitalize()
+    for producto in inventario:
+        if producto_remover == producto['NOMBRE']:
+            inventario.remove(producto)
+            print(f"Producto {producto} eliminado del inventario correctamente.")
 
 def guardar_archivo():
     with open('inventario.txt', 'w', encoding='utf-8') as f:
@@ -39,28 +48,29 @@ def guardar_archivo():
             f.write(f"{producto['CODIGO']} - {producto['NOMBRE']} - {producto['CANTIDAD']} - {producto['PRECIO']}\n")
 
 def menu():
-    print("1.-agregrar producto\n 2.- ver poducto\n 3.- modificar producto\n 4.- eliminar producto\n 5.- guardar producto")
-opcion=()
-
-opcion=input("ingrese la opcion que sea elegir")
-if opcion == '1':
-    agregar_producto()
+    print("1. Agregar producto\n2. Ver poducto\n3. modificar producto\n4. Eliminar producto\n5. Guardar inventario\n6.Salir del programa.")
+    opcion=input("ingrese la opcion que sea elegir")
+    if opcion == "1":
+        agregar_producto()
     
-elif opcion == '2':
-    ver_productos()
+    elif opcion == "2":
+        ver_productos()
 
-elif opcion == '3':
-    modificar_producto
+    elif opcion == "3":
+        modificar_producto()
 
-elif opcion == '4':
-    eliminar_producto
+    elif opcion == "4":
+        eliminar_producto()
 
-elif opcion == '5':
-    guardar_archivo
+    elif opcion == "5":
+        guardar_archivo()
 
-else:
-    print("usted eligio una opcion que no esta en el menu porfavor agregrar una opcion que este en el menu del 1 al 5 ")
- 
+    elif opcion == "6":
+        print("************************")
+        print("Saliendo del programa...")
+        print("************************")
+        time.sleep(2)
+        flag = False
 
-
-    
+    else:
+        print("Usted eligió una opción que no esta en el menú, porfavor ingrese una opcion que este en el menu del 1 al 6 ")
