8f163ca852fce1a0527e9454d753705de5c42034
beibanezv <166031898+beibanezv@users.noreply.github.com>
2024-06-24 12:05:07 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import funciones
+import time
 
 print("--- Bienvenido al programa de gestión de inventario de la tienda 'SCOOBY DOO' ---")
 while True:
     menu_salir = input("Ingrese 'm' si desea abrir el menú de opciones o ingrese 'x' para salir del programa.")
 
     if menu_salir == "m":
+        flag = False
         funciones.menu()
 
     elif menu_salir == "x":
-        funciones.salir()
+        print("************************")
+        print("Saliendo del programa...")
+        print("************************")
+        time.sleep(2)
+        flag = False
+        break
